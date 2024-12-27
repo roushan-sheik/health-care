1c4adac3ad4d2d3fcc6e3fc36b197ec5a4bc0d70
roushan-sheik <roushansheik@gmail.com>
2024-12-27 08:52:47 +0600

    add: multiple fields condition search

--- src/app/modules/Admin/admin.service.ts
+++ src/app/modules/Admin/admin.service.ts
@@ -2,6 +2,8 @@ import { Prisma } from "@prisma/client";
 import { prisma } from "../../lib/database/prisma.client";
 
 export const getAdminsDataFromDB = async (params: any) => {
+  const { searchTerm, ...filterData } = params;
+
   const andCondition: Prisma.AdminWhereInput[] = [];
   const adminSearchAbleFields = ["name", "email"];
 
@@ -17,6 +19,19 @@ export const getAdminsDataFromDB = async (params: any) => {
       }),
     });
   }
+  // multiple fields condition search
+  if (Object.keys(filterData).length > 0) {
+    andCondition.push({
+      AND: Object.keys(filterData).map((key) => {
+        return {
+          [key]: {
+            equals: filterData[key],
+          },
+        };
+      }),
+    });
+  }
+
   const whereCondition: Prisma.AdminWhereInput = { AND: andCondition };
   const result = await prisma.admin.findMany({
     where: whereCondition,
