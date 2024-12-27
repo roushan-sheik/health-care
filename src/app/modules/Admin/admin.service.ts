7aa238b4730cbb6911ca8417a02c327c0c504db8
roushan-sheik <roushansheik@gmail.com>
2024-12-27 08:36:21 +0600

    add: efficient search query

--- src/app/modules/Admin/admin.service.ts
+++ src/app/modules/Admin/admin.service.ts
@@ -1,24 +1,27 @@
+import { Prisma } from "@prisma/client";
 import { prisma } from "../../lib/database/prisma.client";
 
 export const getAdminsDataFromDB = async (params: any) => {
-  return await prisma.admin.findMany({
-    where: {
-      OR: [
-        {
-          name: {
+  const andCondition: Prisma.AdminWhereInput[] = [];
+  const adminSearchAbleFields = ["name", "email"];
+
+  if (params.searchTerm) {
+    andCondition.push({
+      OR: adminSearchAbleFields.map((field: string) => {
+        return {
+          [field]: {
             contains: params.searchTerm,
             mode: "insensitive",
           },
-        },
-        {
-          email: {
-            contains: params.searchTerm,
-            mode: "insensitive",
-          },
-        },
-      ],
-    },
+        };
+      }),
+    });
+  }
+  const whereCondition: Prisma.AdminWhereInput = { AND: andCondition };
+  const result = await prisma.admin.findMany({
+    where: whereCondition,
   });
+  return result;
 };
 
 export const adminService = { getAdminsDataFromDB };
