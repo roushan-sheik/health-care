673f91592a074c7949e962c76a6704510bedab4c
roushan-sheik <roushansheik@gmail.com>
2025-01-03 10:48:16 +0600

    add:  login system

--- src/app/modules/Auth/auth.controller.ts
+++ src/app/modules/Auth/auth.controller.ts
@@ -3,10 +3,26 @@ import AsyncHandler from "../../utils/AsyncHandler";
 import { authServices } from "./auth.service";
 import { SendResponse } from "../../utils/SendResponse";
 import { StatusCodes } from "http-status-codes";
+import { access } from "fs";
 
 const loginUser = AsyncHandler(async (req: Request, res: Response) => {
   const result = await authServices.loginUser(req.body);
-  SendResponse(res, { statusCode: StatusCodes.OK, data: result });
+  // set accessToken to the cookie
+  res.cookie("refreshToken", result.refreshToken, {
+    secure: process.env.NODE_ENV === "production",
+    httpOnly: true,
+  });
+
+  // send response with accessToken
+
+  SendResponse(res, {
+    statusCode: StatusCodes.OK,
+    message: "Login successful",
+    data: {
+      accessToken: result.accessToken,
+      needPasswordChange: result.needPasswordChange,
+    },
+  });
 });
 
 export const authController = { loginUser };
--- src/app/modules/Auth/auth.service.ts
+++ src/app/modules/Auth/auth.service.ts
@@ -1,6 +1,7 @@
 import bcrypt from "bcrypt";
 import { prisma } from "../../../shared/prisma";
 import { IPayload } from "./auth.interface";
+import jwt from "jsonwebtoken";
 
 const loginUser = async (payload: IPayload) => {
   const user = await prisma.user.findUniqueOrThrow({
@@ -13,7 +14,25 @@ const loginUser = async (payload: IPayload) => {
     payload.password,
     user.password
   );
-  return { user, isPasswordCorrect };
+  if (!isPasswordCorrect) {
+    throw new Error("Invalid email or password");
+  }
+  const accessToken = jwt.sign(
+    { email: user.email, role: user.role },
+    process.env.JWT_ACCESS_TOKEN_SECRET as string,
+    { algorithm: "HS256", expiresIn: "1d" }
+  );
+  const refreshToken = jwt.sign(
+    { email: user.email, role: user.role },
+    process.env.JWT_REFRESH_TOKEN_SECRET as string,
+    { algorithm: "HS256", expiresIn: "15d" }
+  );
+
+  return {
+    accessToken,
+    refreshToken,
+    needPasswordChange: user.needPasswordChange,
+  };
 };
 
 export const authServices = { loginUser };
